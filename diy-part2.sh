9ba8c6292866200519b95d447c4a21beec0cecfe
wangxuanyeah <wang_xuanyeah@163.com>
2022-01-02 21:35:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Install theme
 rm -rf package/lean/luci-theme-argon 
